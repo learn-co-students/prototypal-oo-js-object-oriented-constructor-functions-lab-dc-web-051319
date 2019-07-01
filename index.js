fa9447e0cbb4c232ea3178903c0fa30e4f27881f
Emily Bolten <emilybolten@gmail.com>
2019-07-01 10:06:56 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+//code goes here
+
+//scooter with year, color, model
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+
+//driver with name, age, experience
+function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+
+//PickupLocation with address and city
+function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+}
